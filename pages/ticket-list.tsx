5aa706e225481e4b1cb81ef8c73eea73c39be51b
Matt Baron <mlb007@icloud.com>
2024-05-15 17:12:59 -0400

    Fix build by removing incorrect import

--- pages/ticket-list.tsx
+++ pages/ticket-list.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { GetServerSideProps } from 'next';
 import Layout from '../components/Layout';
-import Post, { PostProps } from '../components/Post';
 import prisma from '../lib/prisma';
 import { Status } from '@prisma/client';
 import { Box, Text, Table, Space } from '@mantine/core';
